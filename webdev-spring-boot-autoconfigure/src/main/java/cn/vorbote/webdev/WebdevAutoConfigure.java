b2f740f1f7bc60b52cda1a017b27d188a3a93eec
vorbote <xinke.shao@outlook.com>
2022-07-15 10:17:55 +0800

    refactor: fit for jdk8

--- webdev-spring-boot-autoconfigure/src/main/java/cn/vorbote/webdev/WebdevAutoConfigure.java
+++ webdev-spring-boot-autoconfigure/src/main/java/cn/vorbote/webdev/WebdevAutoConfigure.java
@@ -4,10 +4,10 @@ import cn.vorbote.simplejwt.AccessKeyUtil;
 import cn.vorbote.simplejwt.choices.JwtAlgorithm;
 import cn.vorbote.web.filter.CorsFilter;
 import cn.vorbote.webdev.cors.CorsProperties;
+import cn.vorbote.webdev.encoding.EncodingProperties;
 import cn.vorbote.webdev.jwt.JwtProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @Configuration
-@EnableConfigurationProperties(value = {JwtProperties.class, CorsProperties.class})
+@EnableConfigurationProperties(value = {JwtProperties.class, CorsProperties.class, EncodingProperties.class})
 public class WebdevAutoConfigure {
 
     private final JwtProperties jwtProperties;
@@ -37,8 +37,8 @@ public class WebdevAutoConfigure {
     @ConditionalOnProperty(name = "vorbote.web-dev.jwt.enabled", havingValue = "true")
     public AccessKeyUtil accessKeyUtil() {
         log.debug("Injecting accessKeyUtil...");
-        var info = this.jwtProperties;
-        var algorithm = info.getAlgorithm();
+        JwtProperties info = this.jwtProperties;
+        JwtAlgorithm algorithm = info.getAlgorithm();
         if (algorithm == null) {
             algorithm = JwtAlgorithm.HS256;
         }
